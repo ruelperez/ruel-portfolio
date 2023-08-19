585f9c94296cec564dc6ca1bc02a334dcf90d54e
ruelperez <karuelflorperezz@gmail.com>
2023-08-19 09:01:22 +0800

    allign image on skills

--- resources/views/livewire/portfolio.blade.php
+++ resources/views/livewire/portfolio.blade.php
@@ -41,7 +41,7 @@
             <div class="ml-3 md:hidden">
                 <i class="fa-solid fa-bars text-white text-xl" onclick="profile()"></i>
             </div>
-            <div class="ml-[5.5vh] md:ml-[7vh]">
+            <div class="ml-[5.5vh] md:ml-[7vh] xl:ml-[12vh]">
                 <div class="logo flex">
                     <h3 class="text-white p-1 ml-10  text-3xl">Ruel</h3><h3 class="text-blue-500 pt-1 text-3xl">Dev</h3>
                 </div>
@@ -82,15 +82,15 @@
     @if($base == 0)
         <div class="text-white md:flex">
             <div class="w-[44vh] md:w-3/5">
-                <div class="flex ml-[5vh] mt-10 md:ml-[11vh]">
+                <div class="flex ml-[5vh] mt-10 md:ml-[11vh] xl:ml-[15vh]">
                     <div class="w-1 h-8 bg-blue-500">
                     </div>
                     <h2 class="text-2xl ml-1">About</h2>
                 </div>
-                <div class="ml-[5vh] md:ml-[11vh] mt-5 md:3-3/5 text-justify">
+                <div class="ml-[5vh] md:ml-[15vh] xl:ml-[20vh] mt-5 md:3-3/5 text-justify">
                     Hello world! I'm Ruel Perez, aspiring to become a Software Engineer. I'm a fresh graduate from <a href="https://www.facebook.com/ACLCCollegeIRIGA" class="text-blue-500" target="_blank">ACLC College of Iriga,</a> class of 2023. I hold a Bachelor's degree in Computer Science. I have experience in various programming languages, I currently learning Vue and React but my specialized programming language is <a href="https://laravel.com/" class="text-blue-500" target="_blank">Laravel PHP framework</a> and its special feature, <a href="https://laravel-livewire.com/" class="text-blue-500" target="_blank">Laravel Livewire.</a>
                 </div>
-                <div class="flex md:flex md:ml-20  ml-[25vh] mt-[20vh] hidden md:block">
+                <div class="flex md:flex md:ml-[15vh] xl:ml-[20vh] ml-[25vh] mt-[20vh] hidden md:block">
                     <div class="bg-stone-900 w-[38vh] text-center pb-4">
                         <i class="fa-solid fa-phone text-blue-500 text-2xl pt-4"></i>
                         <h1 class="text-1xl mt-2">09955415702</h1>
@@ -126,7 +126,7 @@
                     <a href="https://laravel.com/" target="_blank"><i class="fa-brands fa-laravel text-red-500 ml-auto text-7xl"></i></a>
                 </div>
                 <div class="ml-auto">
-                    <a href="https://vuejs.org/guide/introduction.html"><i class="fa-brands fa-vuejs text-green-600 text-7xl"></i></a>
+                    <a href="https://vuejs.org/guide/introduction.html" target="_blank"><i class="fa-brands fa-vuejs text-green-600 text-7xl"></i></a>
                 </div>
             </div>
             <div class="flex w-2/4 ml-auto mr-auto mt-10">
@@ -144,7 +144,6 @@
                 <div class="ml-auto">
                     <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><i class="fa-brands fa-css3-alt text-blue-500 text-7xl"></i></a>
                 </div>
-
             </div>
             <div class="flex w-2/4 ml-auto mr-auto mt-10">
                 <div class="mr-auto">
@@ -154,75 +153,114 @@
                     <a href="https://getbootstrap.com/" target="_blank"><i class="fa-brands fa-bootstrap text-violet-600 text-6xl"></i></a>
                 </div>
             </div>
-            @endif
-            {{-- Project--}}
-            @if($base == 3)
-                <div class="flex ml-[25vh] mt-20 text-white">
-                    <div class="w-1.5 h-10 bg-blue-500">
-                    </div>
-                    <h2 class="text-3xl ml-2">Project</h2>
-                </div>
-                <div class="flex ml-[35vh] mt-10 ">
-                    <div class="inline-block relative">
-                        <img wire:mouseover="hoverIn(1)" wire:mouseout="hoverOut" src="{{asset('image/tabulation.jpg')}}" width="350" class="w-80 h-80 object-cover rounded-lg cursor-pointer">
-                        @if($hover == 1)
-                            <div wire:mouseover="hoverIn(1)" wire:mouseout="hoverOut" class="text-white absolute bottom-[0vh] rounded-lg w-80 h-80 bg-opacity-60 bg-stone-400">
-                                <div class="bg-stone-600 w-40 text-center ml-20 mt-[15vh] px-3 py-3 rounded-2xl">
-                                    <a href="https://github.com/ruelperez/event-tabulation" target="_blank"><img src="{{asset('image/github-icon.jpg')}}" class="rounded-full ml-[6.5vh] cursor-pointer" width="40">Visit Repository</a>
-                                </div>
-                            </div>
-                        @endif
-                    </div>
-                    <div class="inline-block relative ml-10">
-                        <img wire:mouseover="hoverIn(2)" wire:mouseout="hoverOut" src="{{asset('image/kiosk.jpg')}}" width="350" class="w-80 h-80 object-cover rounded-lg cursor-pointer">
-                        @if($hover == 2)
-                            <div wire:mouseover="hoverIn(2)" wire:mouseout="hoverOut" class="text-white absolute bottom-[0vh] rounded-lg w-80 h-80 bg-opacity-60 bg-stone-400">
-                                <div class="bg-stone-600 w-40 text-center ml-20 mt-[15vh] px-3 py-3 rounded-2xl">
-                                    <a href="https://github.com/ruelperez/pre-enrollment" target="_blank"><img src="{{asset('image/github-icon.jpg')}}" class="rounded-full ml-[6.5vh] cursor-pointer" width="40">Visit Repository</a>
-                                </div>
-                            </div>
-                        @endif
-                    </div>
-                    <div class="inline-block relative ml-10">
-                        <img wire:mouseover="hoverIn(3)" wire:mouseout="hoverOut" src="{{asset('image/custodian.jpg')}}" width="350" class="w-80 h-80 object-cover rounded-lg cursor-pointer">
-                        @if($hover == 3)
-                            <div wire:mouseover="hoverIn(3)" wire:mouseout="hoverOut" class="text-white absolute bottom-[0vh] rounded-lg w-80 h-80 bg-opacity-60 bg-stone-400">
-                                <div class="bg-stone-600 w-40 text-center ml-20 mt-[15vh] px-3 py-3 rounded-2xl">
-                                    <a href="https://github.com/ruelperez/custodian" target="_blank"><img src="{{asset('image/github-icon.jpg')}}" class="rounded-full ml-[6.5vh] cursor-pointer" width="40">Visit Repository</a>
-                                </div>
-                            </div>
-                        @endif
-                    </div>
-                </div>
-            @endif
-            {{--  Contact  --}}
-            @if($base == 4)
-                <div class="flex ml-[25vh] mt-20 text-white">
-                    <div class="w-1.5 h-10 bg-blue-500">
-                    </div>
-                    <h2 class="text-3xl ml-2">Contact</h2>
-                </div>
-                <div class="flex ml-[65vh] mt-[10vh] text-white">
-                    <div class="bg-stone-900 w-[38vh] text-center pb-4 ">
-                        <i class="fa-solid fa-phone text-blue-500 text-4xl pt-4 transition-transform hover:scale-125"></i>
-                        <h1 class="text-1xl mt-2 transition-transform hover:scale-125">09955415702</h1>
-                    </div>
-                    <div class="bg-stone-900 w-[38vh] text-center ml-10 pb-4">
-                        <i class="fa-solid fa-envelope text-blue-500 text-4xl pt-4 transition-transform hover:scale-125"></i>
-                        <h1 class="transition-transform hover:scale-125">karuelflorperezz@gmail.com</h1>
-                    </div>
-                </div>
-                <div class="w-[81.5vh] mt-10 ml-[65vh] bg-stone-900 text-white pt-4 pb-4">
-                    <h1 class="text-2xl text-center">Connect Us</h1>
-                    <div class="flex ml-[23vh] pt-3 pb-3">
-                        <a href="https://www.facebook.com/ruel.perez.5473894" target="_blank"><i class="fa-brands fa-facebook text-blue-600 text-4xl pt-4 transition-transform hover:scale-125 cursor-pointer"></i></a>
-                        <a href="https://github.com/ruelperez" target="_blank"><i class="fa-brands fa-github text-4xl pt-4 transition-transform hover:scale-125 ml-10 cursor-pointer"></i></a>
-                        <a href="https://www.instagram.com/ruelperez_matt/" target="_blank"><i class="fa-brands fa-instagram text-4xl pt-4 transition-transform hover:scale-125 ml-10 text-pink-600 cursor-pointer"></i></a>
-                        <a href="https://www.tiktok.com/@ruueel?_t=8eS3fEWRAqQ&_r=1" target="_blank"><i class="fa-brands fa-tiktok text-4xl pt-4 transition-transform hover:scale-125 ml-10 cursor-pointer"></i></a>
-                    </div>
-                </div>
-            @endif
         </div>
+        <div class="md:block hidden">
+            <div class="flex ml-[5vh] mt-10 md:ml-[11vh] text-white">
+                <div class="w-1 h-8 bg-blue-500">
+                </div>
+                <h2 class="text-2xl ml-2">Skills</h2>
+            </div>
+            <div class="justify-between flex w-[60vh] ml-auto mr-auto mt-12">
+                <div>
+                    <a href="https://laravel.com/" target="_blank"><i class="fa-brands fa-laravel text-red-500 ml-auto text-8xl"></i></a>
+                </div>
+                <div>
+                    <a href="https://vuejs.org/guide/introduction.html" target="_blank"><i class="fa-brands fa-vuejs text-green-600 text-8xl"></i></a>
+                </div>
+                <div>
+                    <a href="https://vuejs.org/guide/introduction.html"><i class="fa-brands fa-react text-blue-500 text-8xl"></i></a>
+                </div>
+            </div>
+            <div class="justify-between flex w-[60vh] ml-auto mr-auto mt-8">
+                <div>
+                    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank"><i class="fa-brands fa-square-js text-yellow-300 text-8xl"></i></a>
+                </div>
+                <div >
+                    <a  href="https://developer.mozilla.org/en-US/docs/Web/HTML" target="_blank"><i class="fa-brands fa-html5 text-orange-500 text-8xl"></i></a>
+                </div
+                    <a href="https://developer.mozilla.org/en-US/docs/Web/CSS" target="_blank"><i class="fa-brands fa-css3-alt text-blue-500 text-8xl"></i></a>
+                </div>
+            </div>
+            <div class="justify-between flex w-[60vh] ml-auto hidden mr-auto mt-8">
+                <div>
+                    <a href="https://tailwindcss.com/docs/guides/laravel" target="_blank"><img src="https://api.iconify.design/devicon:tailwindcss.svg" width="100"/></a>
+                </div>
+                <div>
+                    <a href="https://getbootstrap.com/" target="_blank"><i class="fa-brands fa-bootstrap text-violet-600 mr-20 text-7xl"></i></a>
+                </div>
+                <div>
+                </div>
+            </div>
+        </div>
+        @endif
+        {{-- Project--}}
+        @if($base == 3)
+            <div class="flex ml-[5vh] mt-10 md:ml-[11vh] xl:ml-[15vh] text-white">
+                <div class="w-1 h-8 bg-blue-500">
+                </div>
+                <h2 class="text-2xl ml-2">Project</h2>
+            </div>
+            <div class="ml-[5vh] mr-[5vh] mt-5 ">
+                <div class="inline-block relative">
+                    <img wire:mouseover="hoverIn(1)" wire:mouseout="hoverOut" src="{{asset('image/tabulation.jpg')}}" class="w-[100vh] h-40 object-cover rounded-lg cursor-pointer">
+                    @if($hover == 1)
+                        <div wire:mouseover="hoverIn(1)" wire:mouseout="hoverOut" class="text-white absolute bottom-[0vh] rounded-lg w-80 h-80 bg-opacity-60 bg-stone-400">
+                            <div class="bg-stone-600 w-40 text-center ml-20 mt-[15vh] px-3 py-3 rounded-2xl">
+                                <a href="https://github.com/ruelperez/event-tabulation" target="_blank"><img src="{{asset('image/github-icon.jpg')}}" class="rounded-full ml-[6.5vh] cursor-pointer" width="40">Visit Repository</a>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+                <div class="inline-block relative ml-10">
+                    <img wire:mouseover="hoverIn(2)" wire:mouseout="hoverOut" src="{{asset('image/kiosk.jpg')}}" width="350" class="w-80 h-80 object-cover rounded-lg cursor-pointer">
+                    @if($hover == 2)
+                        <div wire:mouseover="hoverIn(2)" wire:mouseout="hoverOut" class="text-white absolute bottom-[0vh] rounded-lg w-80 h-80 bg-opacity-60 bg-stone-400">
+                            <div class="bg-stone-600 w-40 text-center ml-20 mt-[15vh] px-3 py-3 rounded-2xl">
+                                <a href="https://github.com/ruelperez/pre-enrollment" target="_blank"><img src="{{asset('image/github-icon.jpg')}}" class="rounded-full ml-[6.5vh] cursor-pointer" width="40">Visit Repository</a>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+                <div class="inline-block relative ml-10">
+                    <img wire:mouseover="hoverIn(3)" wire:mouseout="hoverOut" src="{{asset('image/custodian.jpg')}}" width="350" class="w-80 h-80 object-cover rounded-lg cursor-pointer">
+                    @if($hover == 3)
+                        <div wire:mouseover="hoverIn(3)" wire:mouseout="hoverOut" class="text-white absolute bottom-[0vh] rounded-lg w-80 h-80 bg-opacity-60 bg-stone-400">
+                            <div class="bg-stone-600 w-40 text-center ml-20 mt-[15vh] px-3 py-3 rounded-2xl">
+                                <a href="https://github.com/ruelperez/custodian" target="_blank"><img src="{{asset('image/github-icon.jpg')}}" class="rounded-full ml-[6.5vh] cursor-pointer" width="40">Visit Repository</a>
+                            </div>
+                        </div>
+                    @endif
+                </div>
+            </div>
+        @endif
+        {{--  Contact  --}}
+        @if($base == 4)
+            <div class="flex ml-[25vh] mt-20 text-white">
+                <div class="w-1.5 h-10 bg-blue-500">
+                </div>
+                <h2 class="text-3xl ml-2">Contact</h2>
+            </div>
+            <div class="flex ml-[65vh] mt-[10vh] text-white">
+                <div class="bg-stone-900 w-[38vh] text-center pb-4 ">
+                    <i class="fa-solid fa-phone text-blue-500 text-4xl pt-4 transition-transform hover:scale-125"></i>
+                    <h1 class="text-1xl mt-2 transition-transform hover:scale-125">09955415702</h1>
+                </div>
+                <div class="bg-stone-900 w-[38vh] text-center ml-10 pb-4">
+                    <i class="fa-solid fa-envelope text-blue-500 text-4xl pt-4 transition-transform hover:scale-125"></i>
+                    <h1 class="transition-transform hover:scale-125">karuelflorperezz@gmail.com</h1>
+                </div>
+            </div>
+            <div class="w-[81.5vh] mt-10 ml-[65vh] bg-stone-900 text-white pt-4 pb-4">
+                <h1 class="text-2xl text-center">Connect Us</h1>
+                <div class="flex ml-[23vh] pt-3 pb-3">
+                    <a href="https://www.facebook.com/ruel.perez.5473894" target="_blank"><i class="fa-brands fa-facebook text-blue-600 text-4xl pt-4 transition-transform hover:scale-125 cursor-pointer"></i></a>
+                    <a href="https://github.com/ruelperez" target="_blank"><i class="fa-brands fa-github text-4xl pt-4 transition-transform hover:scale-125 ml-10 cursor-pointer"></i></a>
+                    <a href="https://www.instagram.com/ruelperez_matt/" target="_blank"><i class="fa-brands fa-instagram text-4xl pt-4 transition-transform hover:scale-125 ml-10 text-pink-600 cursor-pointer"></i></a>
+                    <a href="https://www.tiktok.com/@ruueel?_t=8eS3fEWRAqQ&_r=1" target="_blank"><i class="fa-brands fa-tiktok text-4xl pt-4 transition-transform hover:scale-125 ml-10 cursor-pointer"></i></a>
+                </div>
+            </div>
+        @endif
+    </div>
 </div>
 
 <script>
